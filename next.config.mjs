d9cb71cbbeb52e5d9857cc9b8db390aabd6fea0f
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2026-07-20 00:43:50 +0530

    skeleton finished

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.40']
+  allowedDevOrigins: ['192.168.1.40'],
+  output: 'export'
 };
 
 export default nextConfig;
